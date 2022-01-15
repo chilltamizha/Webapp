023bb8bd282b34560e22ff44ad0da726767d2950
cobrax <prahadeesh2017@gmail.com>
2022-01-15 10:51:46 +0530

    project updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from turtle import title
+
 from flask import Flask , render_template
 import intro
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html',title='my website')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
